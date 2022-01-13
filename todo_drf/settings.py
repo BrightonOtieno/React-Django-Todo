03c2821fff640e371561f1dc7f90d23d66a31dc0
brighton hoods <brightonmajiwa@gmail.com>
2022-01-13 20:02:32 +0300

    django_heroku package

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -1,5 +1,5 @@
 import os
-
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +126,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
+
+django_heroku.settings(locals())
